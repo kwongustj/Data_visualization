3af45c69e3480cd50e45fe8f7e0aa530eeb2af82
kwongustj <62468086+kwongustj@users.noreply.github.com>
2020-12-10 21:43:04 +0900

    Delete graph_2.py

--- graph_2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd  
-import matplotlib as mpl       
-import matplotlib.pyplot as plt
-
-df8=pd.read_csv('roadkill.csv',sep=',')
-plt.rc('font', family='NanumGothic') # For Windows
-animals=df8.groupby('노선')['노선'].count()
-print(animals)
-animals.plot(kind = 'bar')
